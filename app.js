67a6769e4aceb7d9b65123b97ca5da60031ba77e
Olowonyo Olawale <31905212+Olawale4444@users.noreply.github.com>
2020-01-06 22:09:53 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+//console
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./theNotes.js');
+
+console.log('Result:', notes.add(9, -2));
---
+++ theNotes.js
@@ -0,0 +1,10 @@
+console.log('Starting theNotes.js');
+
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New note';
+};
+
+module.exports.add = (a, b) => {
+  return a + b;
+};
